c358679a5ad4f211885664233ba0549eb0341660
kasp89s <kasp89s@gmail.com>
2021-07-11 20:00:31 +0300

    1) Проинсталировать базовый функционал регистрации/авторизации пользователя 2) Добавить необходимость подтверждения имейл адреса (отправка письма со ссылкой для подтверждения имейла). Можно использовать готовые решения, можно написать самому

---
+++ src/app/Events/UserRegistrationLog.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class UserRegistrationLog
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($user)
+    {
+        Log::info($message);
+        var_dump($user); exit();
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
